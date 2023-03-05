174fd1232238464e925f1fcf10a1bc9fb826daae
berryc95 <21972449+berryc95@users.noreply.github.com>
2023-03-05 10:41:32 +0000

    Corrections to x-axis

--- dataset.py
+++ dataset.py
@@ -4,28 +4,26 @@
 #Above command allows you to run interactive jupyter for graph
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Great functions to create dataset
 def get_dataset(size):
     # Create Fake Dataset
     df = pd.DataFrame()
-    df['size'] = np.random.choice(['big', 'medium', 'small'], size)
-    df ['age'] = np.random.randint(1, 50, size)
-    df ['team' ] = np.random.choice(['red', 'blue', 'yellow', 'green'], size)
-    df['win'] = np.random.choice(['yes', 'no'], size)
+    #df['size'] = np.random.choice(['big', 'medium', 'small'], size)
+    #df ['team' ] = np.random.choice(['red', 'blue', 'yellow', 'green'], size)
+    #df['win'] = np.random.choice(['yes', 'no'], size)
     dates = pd.date_range('2020-01-01', '2022-12-31')
-    df['date'] = np.random.choice(dates, size)
-    df['prob'] = np.random.uniform(0, 1, size)
+    #df['date'] = np.random.choice(dates, size)
+    df['date'] = (2015,2016,2017,2018)
+    df['friends'] = (2,3,5,7)
+    df['moles'] = np.random.randint(1, 7, size)
     return df
 
 df = get_dataset(4)
-
-#y = {'f(x)': [2,10,6,8], 'g(x)': [4,2,-2,6]}
-#x = [1,2,3,4]
-x = df['date']
-z = df['age']
-y = [1,2,3,4]
-graph = pd.DataFrame(y,x)
-graph.plot(kind='line', grid=True, title="Ages on Dates", ylabel="my y title", xlabel="my x title")#, xlim = (1,4))
-#z= "WAIT HERE"
+randomnumbertest = np.random.randint(20, 50)
+graph =df
+#specify x axis 
+graph.plot(kind='line', x='date', grid=True, title="Ages on Dates", ylabel="Life Indicators", xlabel="Date", ylim = (1,10))
+#plt.show() #if not using jupyter
 # %%
